88a05dc394c0962470f00c14b454f9ad84315518
Isidrito D. Migriño Jr. <sidmiggy@gmail.com>
2024-10-23 21:33:21 +0800

    Migriño.py

---
+++ SEMI FINAL EXAM/Migriño.py
@@ -0,0 +1,29 @@
+#semi-finals exam for CP101-MWF-5:30-6:30PM
+#author : Isidrito D. Migriño Jr.
+
+print("====") 
+print("    ~ Record Keeping App ~")
+print("====")
+print("Available Operators:")
+print("A) Add Data\t\tB) Delete Data")
+print("C) End)")
+selection = str(input("Select an option [A,B,C,]: "))
+
+try:
+    file = open("record.txt", "r")
+except FileNotFoundError:
+    file = open("record.txt", "x")
+
+if selection.upper() =="A":
+    var1 = str(input("Enter Lastname: "))
+    var2 = str(input("Enter Firstname: "))
+    file = open("record.txt", "a")
+    file.write(f"\n{var1}, {var2},")
+    file.close()
+elif selection.upper() =="B":
+    print("No records found.")
+    file = open("record.txt", "r+")
+    file.truncate(0)
+    file.close()
+elif selection.upper() == "C":
+    print("Thank you")
